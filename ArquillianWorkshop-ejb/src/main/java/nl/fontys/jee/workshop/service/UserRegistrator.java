17cd4aa7cfd7680ee1427f22a4dcae943681e0ad
Sebastiaan <sheijmann@xs4all.nl>
2014-12-14 16:23:54 +0100

    implement begin task 1

---
+++ ArquillianWorkshop-ejb/src/main/java/nl/fontys/jee/workshop/service/UserRegistrator.java
@@ -0,0 +1,14 @@
+package nl.fontys.jee.workshop.service;
+
+/**
+ * Service class which is responsible for registering users.
+ *
+ * @author Sebastiaan Heijmann
+ */
+public class UserRegistrator {
+
+  public String register(String username) {
+    return username + " has been successfully registered";
+  }
+
+}
---
+++ ArquillianWorkshop-ejb/src/test/java/nl/fontys/jee/workshop/service/UserRegistratorTest.java
@@ -0,0 +1,28 @@
+package nl.fontys.jee.workshop.service;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * First test with arquillian.
+ * <p>
+ * Only tests for:
+ * <ul>
+ * <li>Context Dependency Injection properly setup.
+ * </ul>
+ *
+ * @author Sebastiaan Heijmann
+ */
+public class UserRegistratorTest {
+
+
+  //TODO Create deployment method.
+
+  //TODO Inject UserRegistrator.
+
+  // TODO Test if CDI is working.
+  @Test
+  public void should_inject_a_user_registrator() {
+    assertFalse("todo", true);
+  }
+}

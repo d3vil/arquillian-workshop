0fddfc7cf2f1d46273a3c0550fa77f5a6f82791a
Sebastiaan <sheijmann@xs4all.nl>
2014-12-14 19:14:31 +0100

    ...

---
+++ ArquillianWorkshop-ejb/src/main/java/nl/fontys/jee/workshop/model/User.java
@@ -0,0 +1,18 @@
+package nl.fontys.jee.workshop.model;
+
+/**
+ * Representation of a user.
+ *
+ * @author Sebastiaan Heijmann
+ */
+public class User {
+  private String username;
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+}
--- ArquillianWorkshop-ejb/src/main/java/nl/fontys/jee/workshop/service/UserRegistrator.java
+++ ArquillianWorkshop-ejb/src/main/java/nl/fontys/jee/workshop/service/UserRegistrator.java
@@ -1,14 +1,18 @@
 package nl.fontys.jee.workshop.service;
 
+import javax.ejb.Stateless;
+import nl.fontys.jee.workshop.model.User;
+
 /**
  * Service class which is responsible for registering users.
  *
  * @author Sebastiaan Heijmann
  */
+@Stateless
 public class UserRegistrator {
 
-  public String register(String username) {
-    return username + " has been successfully registered";
+  public String register(User user) {
+    return user.getUsername() + " has been successfully registered";
   }
 
 }

8334bdacd4c34e2dd963e7658b93104604761792
Sebastiaan <sheijmann@xs4all.nl>
2014-12-14 19:11:38 +0100

    ...

--- ArquillianWorkshop-ejb/src/test/java/nl/fontys/jee/workshop/service/UserRegistratorTest.java
+++ ArquillianWorkshop-ejb/src/test/java/nl/fontys/jee/workshop/service/UserRegistratorTest.java
@@ -1,11 +1,15 @@
 package nl.fontys.jee.workshop.service;
 
 import javax.inject.Inject;
+import nl.fontys.jee.workshop.model.User;
+import nl.fontys.jee.workshop.repository.UserRepository;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
@@ -25,10 +29,12 @@ import org.junit.runner.RunWith;
 public class UserRegistratorTest {
 
   @Deployment
-  public static JavaArchive createDeployment() {
-    return ShrinkWrap.create(JavaArchive.class)
-            .addClass(UserRegistrator.class)
-            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+  public static Archive<?> createTestArchive() {
+    return ShrinkWrap.create(WebArchive.class, "test.war")
+            .addClasses(User.class, UserRegistrator.class ,UserRepository.class)
+            .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+            .addAsWebInfResource("test-ds.xml", "test-ds.xml");
   }
 
   @Inject
@@ -42,9 +48,11 @@ public class UserRegistratorTest {
   //TODO
   @Test
   public void should_register_a_user() {
+    User user = new User();
+    user.setUsername("User");
     assertEquals("should return success message",
             "User has been successfully registered",
-            registrator.register("User"));
+            registrator.register(user));
   }
 
 }
